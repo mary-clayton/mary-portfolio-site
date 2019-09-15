fa1754706eb4bc8b341c4b0d7f14559b8e08946f
Mary <mary.clayton1@hotmail.com>
2019-09-15 15:29:57 -0700

    changes 3

--- myapp/src/footer.js
+++ myapp/src/footer.js
@@ -1,8 +1,11 @@
 import React from "react";
+import ReactContactForm from 'react-mail-form';
+
 export default function Footer () {
     return (
         <div>
-            <h2>Projects</h2>
+            <h2>Contact</h2>
+            <ReactContactForm className="contact" to="mary.clayton@mary-cathlean.com" />
         </div>
     )
 }
